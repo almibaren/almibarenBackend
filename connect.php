52662e1b4ccba6ed6679f14578a6f266765b8e96
almibaren <almibaren@gmail.com>
2018-11-08 13:49:19 +0100

    Added Login Service

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$conexion=new mysqli('192.168.6.141','webservice','Almi123','almibaren');
-$conexion2=new mysqli('192.168.6.141','webservice','Almi123','almibaren');
-$conexion3=new mysqli('192.168.6.141','webservice','Almi123','almibaren');
+$conexion=new mysqli('192.168.6.141','root','Almi123','almibaren');
+//$conexion2=new mysqli('192.168.6.141','webservice','Almi123','almibaren');
+//$conexion3=new mysqli('192.168.6.141','webservice','Almi123','almibaren');
 if(mysqli_connect_error()){
-    die("ERROR DE CONEXION: " . mysqli_connect_error());
+  die(mysqli_connect_errno());
 }
--- users/login/login.php
+++ users/login/login.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sergio
- * Date: 7/11/18
- * Time: 11:39
- */
+
+$post=file_get_contents('php://input');
+
+$request=json_decode($post);
+
+include('../../connect.php');
+
+$user=$request->user;
+$passwd=$request->passwd;
+
+if($user!="" && $passwd!=""){
+    $sql = "SELECT * FROM cliente WHERE 'user'=? AND 'passwd'=?";
+    $res=$conexion->prepare($sql);
+    $res->bind_param('ss',$user,$passwd);
+    $res->execute();
+
+    $res->bind_result($dni,$nombre,$apel1,$apel2,$user,$passwd,$imagen);
+    $cliente=array();
+    while($res->fetch()){
+        $cliente=array('dni'=>$dni,'nombre'=>$nombre,'apellido1'=>$apel1,'apellido2'=>$apel2,'user'=>$user,'passwd'=>$passwd,'imagen'=>$imagen);
+    }
+    $datos=json_encode($cliente);
+    echo $datos;
+}
